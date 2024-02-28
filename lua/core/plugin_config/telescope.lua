6a086e97a75f2b5403fd91eeda7b16a35278f2cc
apooravm <medal.apoorav@gmail.com>
2024-02-28 13:54:26 +0530

    lsp changes | telescope key remaps

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,6 +1,9 @@
 local builtin = require('telescope.builtin')
 
+-- Both '<Leader>pf' and 'Enter Key' will bring up the find_files menu
 vim.keymap.set('n', '<Leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<CR>', builtin.find_files, {})
+
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 vim.keymap.set('n', '<Leader>ps', builtin.live_grep, {})
