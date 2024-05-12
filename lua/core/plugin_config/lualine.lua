ac079e3ca61e681d4848d224864c722b0ec5d508
apooravm <medal.apoorav@gmail.com>
2024-05-12 18:29:42 +0530

    Telescope and NvimTree following .gitignore. Need them to ignore .ignore. Dunno how for now

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,14 +1,14 @@
-local custom_horizon = require'lualine.themes.horizon'
+local custom_horizon = require("lualine.themes.horizon")
 
-custom_horizon.normal.a.bg = 'b6fc03'
-custom_horizon.command.a.bg = 'f76a05'
-custom_horizon.visual.a.bg = '8726eb'
+custom_horizon.normal.a.bg = "b6fc03"
+custom_horizon.command.a.bg = "f76a05"
+custom_horizon.visual.a.bg = "8726eb"
 
 local function custom_time_component()
 	return os.date("%H:%M")
 end
 
-require('lualine').setup {
+require("lualine").setup({
 	options = {
 		-- 
 		-- 
@@ -21,8 +21,8 @@ require('lualine').setup {
 		-- }
 		icons_enabled = true,
 		theme = custom_horizon,
-		component_separators = { left = '', right = '/'},
-		section_separators = { left = '', right = ''},
+		component_separators = { left = "", right = "/" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
@@ -34,26 +34,28 @@ require('lualine').setup {
 			statusline = 1000,
 			tabline = 1000,
 			winbar = 1000,
-		}
+		},
 	},
 	sections = {
-    lualine_a = {'mode'},
-    lualine_b = {{'branch', color={bg='#ff77a2', fg='#0f121c'}, separator={left='\\', right=''}}},
-    lualine_c = {{'filename', path=3}},
-    lualine_x = {'fileformat', { 'filetype', icon_only = true }, { custom_time_component }},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
+		lualine_a = { "mode" },
+		-- color={bg='#ff77a2', fg='#0f121c'},
+		-- lualine_b = {{'branch', color={bg='#ff77a2', fg='#0f121c'}, separator={left='\\', right=''}}},
+		lualine_b = { { "branch", separator = { left = "\\", right = "" } } },
+		lualine_c = { { "filename", path = 3 } },
+		lualine_x = { "fileformat", { "filetype", icon_only = true }, { custom_time_component } },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "encoding", "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {},
+})
--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,10 +1,22 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
+local telescope = require("telescope")
+
+telescope.setup({
+	defaults = {
+		file_ignore_patterns = {
+			"node_modules/.*",
+			".git/.*",
+		},
+		finder_command = { "--no-ignore", "!.git" },
+		no_ignore = true,
+	},
+	additional_args = function(args)
+		return vim.list_extend(args, { "--hidden", "--no-ignore" })
+	end,
+})
 
 -- Both '<Leader>pf' and 'Enter Key' will bring up the find_files menu
-vim.keymap.set('n', '<Leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<CR>', builtin.find_files, {})
-
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-
-vim.keymap.set('n', '<Leader>ps', builtin.live_grep, {})
-
+vim.keymap.set("n", "<Leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<CR>", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<Leader>ps", builtin.live_grep, {})
