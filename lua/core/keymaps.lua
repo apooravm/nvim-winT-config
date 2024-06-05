1945915eb950a4186215337c1f8450d9e32b7952
apooravm <medal.apoorav@gmail.com>
2024-06-05 16:43:24 +0530

    error message

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -21,7 +21,7 @@ vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 
 -- Enable mouse support
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Other
 vim.opt.showcmd = true
@@ -29,30 +29,32 @@ vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.autoread = true
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set <Ctrl> as the leader key
 -- vim.g.mapleader = '<Ctrl>'
 
 -- Map <Leader>p to paste from system clipboard
-vim.api.nvim_set_keymap('n', '<Leader>p', '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<Leader>p', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>p", '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Leader>p", '"+y', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<A-S-Down>', ':t.<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.api.nvim_set_keymap("n", "<A-S-Down>", ":t.<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
--- nvim-tree 
+-- nvim-tree
 vim.g.loaded_newtw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
-vim.keymap.set('n', '<c-m>', ':NvimTreeFocus<CR>')
+vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
+vim.keymap.set("n", "<c-m>", ":NvimTreeFocus<CR>")
 
 -- Move lines of code around with K, J
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- 'space+l' to show a dialog box containing the error. Useful for when the error line is too long
+-- vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
@@ -64,4 +66,3 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 --^ Remap CTRL-` to switch between files
 -- vim.api.nvim_set_keymap('n', '<C-^>', '<C-`>', { noremap = true, silent = true })
-
